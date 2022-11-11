30a7455863c7f8d7c2a005a78d52534e7edf1fc2
Enaz Banks <enazrbanks@gmail.com>
2022-11-11 14:29:03 +1100

    Score page created

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,8 @@ class GamesController < ApplicationController
 
   def new
   end
+
+  def score
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/new', to: 'games#new'
+  get '/score', to: 'games#score'
 end
